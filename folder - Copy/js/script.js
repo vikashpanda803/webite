abe0c04adce541807746deb1995f05778d556fec
vikashpanda803 <131515971+vikashpanda803@users.noreply.github.com>
2024-08-06 00:36:01 +0530

    Add files via upload

---
+++ folder - Copy/js/script.js
@@ -0,0 +1,60 @@
+// navbar toggling
+const navbarShowBtn = document.querySelector(".navbar-show-btn");
+const navbarCollapseDiv = document.querySelector(".navbar-collapse");
+const navbarHideBtn = document.querySelector(".navbar-hide-btn");
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+}
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+}
+var modal = document.getElementById("llm");
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+function hh() {
+  document.getElementById("llm").style.display = "block";
+}
+function close() {
+  document.getElementById("llm").style.display = "none";
+}
+
+navbarShowBtn.addEventListener("click", function () {
+  navbarCollapseDiv.classList.add("navbar-show");
+});
+navbarHideBtn.addEventListener("click", function () {
+  navbarCollapseDiv.classList.remove("navbar-show");
+});
+
+// changing search icon image on window resize
+window.addEventListener("resize", changeSearchIcon);
+function changeSearchIcon() {
+  let winSize = window.matchMedia("(min-width: 1200px)");
+  if (winSize.matches) {
+    document.querySelector(".search-icon img").src = "images/search-icon.png";
+  } else {
+    document.querySelector(".search-icon img").src =
+      "images/search-icon-dark.png";
+  }
+}
+changeSearchIcon();
+
+// stopping all animation and transition
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
